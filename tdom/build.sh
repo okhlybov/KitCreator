676b476f22304c8e064d8d5e59118e90ca4c9019
Oleg A. Khlybov <fougas@mail.ru>
2023-11-09 13:49:20 +0500

    tdom update to 0.9.3

--- tdom/build.sh
+++ tdom/build.sh
@@ -2,6 +2,6 @@
 
 # BuildCompatible: KitCreator
 
-version='0.9.1'
+version='0.9.3'
 url="http://tdom.org/downloads/tdom-${version}-src.tgz"
-sha256='3b1f644cf07533fe4afaa8cb709cb00a899d9e9ebfa66f4674aa2dcfb398242c'
+sha256='b46bcb6750283bcf41bd6f220cf06e7074752dc8b9a87a192bd81e53caad53f9'
